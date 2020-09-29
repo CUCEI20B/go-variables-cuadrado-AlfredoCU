490dff64a3882a8e648b7504a7ce69245b8cc509
AlfredoCU <alfredo_11_cu@hotmail.com>
2020-09-29 13:05:11 -0500

    Calcular el Area de un cuadrado.

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
+// Importando nuestras bibliotecas.
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+// Creamos una funcion principal.
+func main() {
+	// Declaramos una variable de tipo float64.
+	var side float64
+	
+	fmt.Scanf("%f", &side) // Tomamos de entrada un valor numérico.
+	// Formula para sacar el área de un cuadrado.
+	res := side * side
+	fmt.Println(res)
 }
