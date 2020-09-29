f7f9853009c39f80358f06a1f9add67a0b58adca
AlfredoCU <alfredo_11_cu@hotmail.com>
2020-09-29 13:09:28 -0500

    Prueba 2 del calculo del cuadradado.

--- main.go
+++ main.go
@@ -6,9 +6,9 @@ import "fmt"
 // Creamos una funcion principal.
 func main() {
 	// Declaramos una variable de tipo float64.
-	var side float64
-	
-	fmt.Scanf("%f", &side) // Tomamos de entrada un valor numérico.
+	var side uint64
+
+	fmt.Scanf("%d", &side) // Tomamos de entrada un valor numérico.
 	// Formula para sacar el área de un cuadrado.
 	res := side * side
 	fmt.Println(res)
